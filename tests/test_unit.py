70b9f846602686ff0a58aac55b18cf1bef2edadf
Gergő <kanyari.gergo@gmail.com>
2024-12-05 10:43:59 +0100

    add function and test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,5 +8,8 @@ class TestMathOperations(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
 
+    def test_add3(self):
+        self.assertEqual(add(1, 2, 3), 6)
+
 if __name__ == '__main__':
     unittest.main()
